85efbcf9b28fa96d6e1b3c85957d6706afb01b72
Tristen Dillon <tristendillon24@outlook.com>
2026-06-15 01:30:13 +0000

    fix root dir thing

--- .devcontainer/start.sh
+++ .devcontainer/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-WORKSPACE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/../../.." && pwd)"
+WORKSPACE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
 if [ -f "$WORKSPACE_DIR/.envrc" ]; then
   direnv allow "$WORKSPACE_DIR" >/dev/null 2>&1 || true
